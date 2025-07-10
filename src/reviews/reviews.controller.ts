f14d179d7f7e76422d252c773dcc201a619bc679
karimsarhan <kimokomono74@gmail.com>
2025-07-10 20:48:00 +0300

    review controller removing the logic to service and use the contrutore to make it usable in the controler

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -13,27 +13,21 @@ import {
 
 import { CreateReviewDto } from './dtos/create-review.dto';
 import { UpdateReviewDto } from './dtos/update-review.dto';
+import { ReviewsService } from './reviews.service';
 
-type Reviews = {
-  id: number;
-  products: string;
-  review: string;
-  rating: number;
-};
+
 
 @Controller('api/reviews')
 export class ReviewsController {
-  private reviews: Reviews[] = [
-    { id: 1, products: 'book', review: 'good', rating: 5 },
-    { id: 2, products: 'pen', review: 'bad', rating: 2 },
-    { id: 3, products: 'laptop', review: 'excellent', rating: 4.5 },
-  ];
+
+    private ReviewsService: ReviewsService = new ReviewsService();
+
 
   //GET: http://localhost:5000/api/reviews
   //GET: ~/api/reviews
   @Get()
   getAllreviews() {
-    return this.reviews;
+    return this.ReviewsService.getAllreviews();
   }
   //post single element
   @Post()
@@ -43,14 +37,8 @@ export class ReviewsController {
   ) {
     // console.log(body);
     //     return body
-    const newReview: Reviews = {
-      id: this.reviews.length + 1,
-      products: body.products,
-      review: body.review,
-      rating: body.rating,
-    };
-    this.reviews.push(newReview);
-    return newReview;
+
+    return this.ReviewsService.createRview(body);
   }
 
   //add multiple array with multiple object
@@ -68,13 +56,8 @@ export class ReviewsController {
 
   @Get('/:id')
   public getReviewById(@Param('id', ParseIntPipe) id: number) {
-    const review = this.reviews.find((r) => r.id === id);
-    if (!review) {
-      throw new NotFoundException('Review not found', {
-        description: 'no reviews found',
-      });
-    }
-    return review;
+   return this.ReviewsService.getReviewById(id);
+ 
   }
 
   @Put(':id')
@@ -83,31 +66,14 @@ export class ReviewsController {
     @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
     body: UpdateReviewDto,
   ) {
-    const index = this.reviews.findIndex((r) => r.id === id);
+   
 
-    if (index === -1) {
-      throw new NotFoundException('Review not found', {
-        description: 'no reviews found',
-      });
-    }
-
-    // Update the existing review in-place
-    this.reviews[index] = {
-      ...this.reviews[index],
-      ...body,
-    };
-
-    return this.reviews[index]; // Return the updated review
+    return this.ReviewsService.updateReview; // Return the updated review
   }
 
   @Delete(':id')
   public deleteReview(@Param('id', ParseIntPipe) id: number) {
-    const review = this.reviews.find((r) => r.id);
-    if (!review) {
-      throw new NotFoundException('Review not found', {
-        description: 'no reviews found',
-      });
-    }
-    return { message: 'product deleted sucessfully' };
+  
+    return this.ReviewsService.deleteReview(id);
   }
 }
