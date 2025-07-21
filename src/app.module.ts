ffa64ec0aa7245c48647f93eb243a234da02378c
karimsarhan <kimokomono74@gmail.com>
2025-07-21 14:55:42 +0300

    connecting with post gres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 @Module({
-  imports: [ProductsModule,  ReviewsModule, UsersModule],
+  imports: [ProductsModule,  ReviewsModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: 'nestjs-app-db',
+    username: 'postgres',
+    password: '123456789',
+    port: 5432,
+    host: 'localhost',
+    synchronize: true,//only for development
+    entities:[]
+
+  })],
 
 
 })
